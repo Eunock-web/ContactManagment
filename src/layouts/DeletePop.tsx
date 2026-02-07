251ec9d7e9a64121dd072e1b3e450cd472b47026
Eunock <izumishinishi7@gmail.com>
2026-02-07 21:27:19 +0100

    feat: Finalisation du pop-up de validation

--- src/layouts/DeletePop.tsx
+++ src/layouts/DeletePop.tsx
@@ -1,22 +1,25 @@
-import { TriangleAlert } from "lucide-react";
+import { TriangleAlert, X } from "lucide-react";
 import Button from "../components/Button";
 
 
 function DeletePop(){
 
     return <>
-        <div>
-            <TriangleAlert size={20} className="" />
+        <div className="border p-5 flex flex-col justify-center items-center w-[21%] border-slate-800 rounded-lg bg-slate-800 gap-3 border-t-5 border-t-red-600 ">
+
+            <TriangleAlert size={55} className=" text-red-700 border border-red-300 bg-red-300 p-3 rounded-full  " />
             
-            <div>
-                <h1> Delete Contact? </h1>
-                <p> Are you sure you want to delete <span>Jane Smith</span>? This action is permanent and cannot be undone. </p>
+            <div className="flex flex-col gap-3 text-center ">
+                <h1 className="font-bold text-2xl text-white"> Delete Contact? </h1>
+                <p className="text-gray-400 "> Are you sure you want to delete <span className="text-white font-bold">Jane Smith</span>? This action is permanent and cannot be undone. </p>
             </div>
 
-            <div>
-                <Button > Cancel </Button>
-                <Button > Delete Permanently </Button>
+            <div className="flex flex-row gap-10">
+                <Button className="text-white border border-gray-400 py-2 px-8 rounded-xl " > Cancel </Button>
+                <Button  className="text-white bg-red-700 py-2 px-2 border-red-600 rounded-xl"> Delete Permanently </Button>
             </div>
+
+            <X className="text-gray-400 absolute top-4 right-4 cursor-pointer hover:white " size={20} />
         </div>
     </>
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import Layout from "./pages/Layout";
 import React from "react";
 import Acceuil from "./pages/Acceuil";
 import NotFound from "./pages/NotFound";
+import DeletePop from "./layouts/DeletePop";
 
 
 
@@ -17,6 +18,11 @@ export const appRouter = createBrowserRouter([
                 path : '/',
                 index : true,
                 element : React.createElement(Acceuil),
+            },
+
+            {
+                path : '/delete',
+                element : React.createElement(DeletePop),
             },
 
             {
